2cd01ed42e0a6c6262c19f566d609cdc8f5038be
Brittni Bertolet <bbertole@nd.edu>
2018-10-12 13:12:48 -0400

    Update Exercise6.R

--- Exercise6.R
+++ Exercise6.R
@@ -5,6 +5,9 @@ iris<-read.csv("iris.csv",stringsAsFactors = FALSE) #Reading in the iris file to
 
 #For Question 1
 head(iris, 10) #print the first 10 rows to the console
+### Brittni: We wanted you to recreate the functionality of head without using it. I took off 0.25 points, because you did still assign the variables. What we were looking for was something like:
+## number=10
+## iris[1:number,]
 
 #For Question 2
 #Print last two rows of last two columns
@@ -12,6 +15,9 @@ iris[c(149,150),c(4,5)] #Row,column--the last two rows are 149 and 150 while the
 
 #Count number of observations for each species
 count(iris,Species) #First the data and then indicate the variable to group the characters by for counting
+### Brittni: Make sure if you're using functions in other packages that you have them loaded in the script (ie library(plyr)).
+### Also, this isn't working on my machine. I think you need to put Species in quotes. Did you use attach() with the Species column prior? 
+### I took off 0.5 points. 
 
 #Rows with sepal length greater than 3.5
 iris[iris$Sepal.Width>3.5,] #Keeps only the data with iris fitting the logic Sepal.Width>3.5
